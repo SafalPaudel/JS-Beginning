fa0274a05c59c4b625b3865e8ca271bf3dfd5c27
SafalPaudel <152069640+SafalPaudel@users.noreply.github.com>
2024-06-12 16:45:57 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs , not browser
+
+console.log(5+5)  // code readability should be high
+
+console.log("Safal")
+
+
+
+let name = "Safal"
+let age = 20
+let isLoggedIn = true
+let state;
+// number  => 2  to the power 53
+// bigint 
+// string => ""
+
+// boolean => true/false
+// null => stand alone value
+// undefined => no value assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
