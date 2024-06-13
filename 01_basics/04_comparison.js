2029263d3a894b4e0ef55949f0ef4d9a3b195cea
SafalPaudel <152069640+SafalPaudel@users.noreply.github.com>
2024-06-13 03:02:00 +0000

    Awesome

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);    // equality
+console.log(null == 0);   // comparisom
+console.log(null >= 0);   // comparison
+
+// comparison and equality works differenly
+// comparison convert null to a number , treating it as 0.
+// Thats why null >= 0 is true  and  null > 0 is false.
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined > 0);
+
+/// ===
+
+console.log("2" === 2);  // false due to different datatypes
+
+//********************Clean Code Is Must******************** */
