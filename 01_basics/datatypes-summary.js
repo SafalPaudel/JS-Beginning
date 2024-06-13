50c4a8ba680b422271c4411dfa758aadf0c469cd
SafalPaudel <152069640+SafalPaudel@users.noreply.github.com>
2024-06-13 13:51:47 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+    // 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+const score  = 100
+const scoreValue  = 100.5
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+// const bigNumber = 34453868383928942n
+
+
+// Reference ( Non-Primitive )
+    // Array , Objects , Functions 
+
+
+    const heroes = ["IronMan", "BatMan", "SpiderMan"]  //Array
+
+ let myObj  = {
+        name:"Safal",
+        age: 20,
+}
+
+    const myFunction = function(){
+        console.log("Hello World")
+    }
+
+    console.log(typeof bigNumber)
+    console.log(typeof outsideTemp)
+    console.log(typeof scoreValue )
+    console.log(typeof myFunction)
+    console.log(typeof heroes )
+    console.log(typeof anotherId )
