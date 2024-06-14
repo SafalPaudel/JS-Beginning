e7d075f38c69add2d1678089a3ea812195e736d0
SafalPaudel <152069640+SafalPaudel@users.noreply.github.com>
2024-06-14 13:34:44 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 500
+// console.log(score);
+
+
+const balance = new Number(300)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8996
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// ++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));     // negative becomes positive
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,3,6))
+// console.log(Math.max(4,3,6))
+
+console.log(Math.random())     // In random value comes betn 0 to 1
+console.log((Math.random()*10) + 1);   
+console.log(Math.floor(Math.random()*10) + 1);   
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * ( max - min + 1)) + min)
+
+
