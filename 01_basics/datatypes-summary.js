8d3985d724d664a9123814275ca13dde9de64695
SafalPaudel <152069640+SafalPaudel@users.noreply.github.com>
2024-06-14 01:55:38 +0000

    Quick

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -37,3 +37,30 @@ console.log(id == anotherId)
     console.log(typeof myFunction)
     console.log(typeof heroes )
     console.log(typeof anotherId )
+
+//***************************************************************
+
+//  Stack  (Primitive) , Heap (Non-Primitive)
+
+let myYoutubename = "SafalPaudel.com"
+
+let anotherName = myYoutubename
+anotherName = "MrBeast"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let user1 = {
+    email : "user@gmail.com",
+    esewa : "user@esewa"
+}
+
+let user2 = user1
+
+user2.email = "safal@google.com"
+console.log(user1.email);
+console.log(user2.email);
+
+
+// In Stack we get copy
+// In Heap we ret references
